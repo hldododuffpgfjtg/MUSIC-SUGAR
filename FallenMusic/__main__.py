cfbe7ddba063a4787bfc106bdd7b47d4a63d0882
hldododuffpgfjtg <129379124+hldododuffpgfjtg@users.noreply.github.com>
2023-06-09 01:48:31 +0300

    Add files via upload

--- FallenMusic/__main__.py
+++ FallenMusic/__main__.py
@@ -58,7 +58,7 @@ async def fallen_startup():
     try:
         await app.send_message(
             SUNAME,
-            f"✯ ѕᴘɪᴅᴇʀ ᴍᴜsɪᴄ ʙᴏᴛ ✯\n\n𖢵 ɪᴅ : `{BOT_ID}`\n𖢵 ɴᴀᴍᴇ : {BOT_NAME}\n𖢵 ᴜsᴇʀɴᴀᴍᴇ : @{BOT_USERNAME}",
+            f"✯ TWH ✯\n\n ɪᴅ : `{BOT_ID}`\n ɴᴀᴍᴇ : {BOT_NAME}\n ᴜsᴇʀɴᴀᴍᴇ : @{BOT_USERNAME}",
         )
     except:
         LOGGER.error(
@@ -68,7 +68,7 @@ async def fallen_startup():
     try:
         await app2.send_message(
             SUNAME,
-            f"✯ ѕᴘɪᴅᴇʀ ᴍᴜsɪᴄ ᴀss ✯\n\n𖢵 ɪᴅ : `{ASS_ID}`\n𖢵 ɴᴀᴍᴇ : {ASS_NAME}\n𖢵 ᴜsᴇʀɴᴀᴍᴇ : @{ASS_USERNAME}",
+            f"✯ TWH ✯\n\n ɪᴅ : `{ASS_ID}`\n ɴᴀᴍᴇ : {ASS_NAME}\n ᴜsᴇʀɴᴀᴍᴇ : @{ASS_USERNAME}",
         )
     except:
         LOGGER.error(
